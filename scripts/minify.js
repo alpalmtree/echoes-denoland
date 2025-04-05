7d15b2c5cce65eab7d1ef3121d100bcc3f2231da
Al <alpalma95@gmail.com>
2025-04-05 19:50:26 +0200

    chore: add minify script

---
+++ scripts/minify.js
@@ -0,0 +1,20 @@
+import * as esbuild from 'esbuild'
+import process from "node:process"
+
+await esbuild.build({
+    entryPoints: [`${process.cwd()}/src/index.js`],
+    bundle: true,
+    outfile: 'dist/emitters.esm.js',
+    minify: true,
+    format: 'esm',
+  })
+
+
+await esbuild.build({
+  entryPoints: [`${process.cwd()}/src/index.js`],
+    bundle: true,
+    outfile: 'dist/emitters.iife.js',
+    minify: true,
+    format: 'iife',
+    globalName: 'emitters',
+  })
